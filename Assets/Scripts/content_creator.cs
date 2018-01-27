c40a816f0801399b4e8b05e1cb456463b119db54
Brendon <brendonmills@gmail.com>
2018-01-27 16:47:02 +1300

    Moved the messages into resources, just for ease

--- Assets/Scripts/content_creator.cs
+++ Assets/Scripts/content_creator.cs
@@ -13,7 +13,16 @@ public class content_creator : MonoBehaviour {
         {
             new content() { content_name = "cabbage_recipe", content_type = content.content_types.image, proper_destination = content.proper_destinations.grandma},
             new content() { content_name = "spam_can", content_type = content.content_types.image, proper_destination = content.proper_destinations.intended},
-            new content() { content_name = "puppy_and_kitten", content_type = content.content_types.image, proper_destination = content.proper_destinations.intended}
+            new content() { content_name = "puppy_and_kitten", content_type = content.content_types.image, proper_destination = content.proper_destinations.intended},
+
+            new content() { content_name = "Banana Shack", content_type = content.content_types.image, proper_destination = content.proper_destinations.cia},
+            new content() { content_name = "Carl's guns", content_type = content.content_types.image, proper_destination = content.proper_destinations.intended},
+            new content() { content_name = "Cocaine Girl", content_type = content.content_types.image, proper_destination = content.proper_destinations.intended},
+            new content() { content_name = "Malicious", content_type = content.content_types.video, proper_destination = content.proper_destinations.intended},
+            new content() { content_name = "Pregante", content_type = content.content_types.image, proper_destination = content.proper_destinations.intended},
+            new content() { content_name = "Shona's Food Blog", content_type = content.content_types.image, proper_destination = content.proper_destinations.cia},
+            new content() { content_name = "Millionaire", content_type = content.content_types.image, proper_destination = content.proper_destinations.intended}
+            //new content() { content_name = "puppy_and_kitten", content_type = content.content_types.image, proper_destination = content.proper_destinations.intended}
 
             // new content here
         };
