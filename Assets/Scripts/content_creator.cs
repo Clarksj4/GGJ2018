15a88f1be47f1a929954b36042e76c34ea7f7112
Brendon <brendonmills@gmail.com>
2018-01-27 15:16:05 +1300

    content creator created

--- Assets/Scripts/content_creator.cs
+++ Assets/Scripts/content_creator.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class content_creator : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    static System.Random rnd = new System.Random();       // Lets keep random random
+    public List<content>  content_items;    // hold our content
+
+    public content_creator()
+    {
+        List<content> content_items = new List<content>
+        {
+            new content() { content_name = "cabbage_recipe", content_type = content.content_types.image, proper_destination = content.proper_destinations.grandma},
+            new content() { content_name = "spam_can", content_type = content.content_types.image, proper_destination = content.proper_destinations.intended},
+            new content() { content_name = "puppy_and_kitten", content_type = content.content_types.image, proper_destination = content.proper_destinations.intended}
+
+            // new content here
+        };
+    }
+
+
+    public content get_me_content() 
+    {
+        int r = rnd.Next(content_items.Count);
+
+        return content_items[r];
+    }
+
+
 }
