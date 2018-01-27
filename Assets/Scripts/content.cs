0e9f75056e82987189fb929963251301a4ad32dd
Stephen <clarksj4@student.op.ac.nz>
2018-01-27 15:04:52 +1300

    Content script downgraded to an ordinary class

--- Assets/Scripts/content.cs
+++ Assets/Scripts/content.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-public class content : MonoBehaviour {
 
-    public enum content_types { image, video, audio, text };
+public class content {
+
+    public enum content_types { image, video, audio };
     public enum proper_destinations { grandma, intended, cia };
 
     public string content_name { get; set; }
     public content_types content_type { get; set; }
     public proper_destinations proper_destination { get; set; }
-    public Image resource { get; set; }
 }
