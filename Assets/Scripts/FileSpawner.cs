e664f2b79073f40312b6cf31e4cc7944a01a6329
Brendon <brendonmills@gmail.com>
2018-01-28 13:38:20 +1300

    Grab a random new message until you have seen each one once, then start again

--- Assets/Scripts/FileSpawner.cs
+++ Assets/Scripts/FileSpawner.cs
@@ -9,10 +9,13 @@ public class FileSpawner : MonoBehaviour
     public List<content> content_items;
     static System.Random rnd = new System.Random();       // Lets keep random random
     private Openable currentFile;
+    private bool[] viewed_message;
+    private int total_viewed =0;
 
     private void Start()
     {
         Invoke("Spawn", 0);
+        viewed_message = new bool[content_items.Count];
     }
 
     public void Spawn()
@@ -33,7 +36,26 @@ public class FileSpawner : MonoBehaviour
 
     public content get_me_content()
     {
+        //choose a random number
         int r = rnd.Next(content_items.Count);
+
+        //check to see if we have already used this response. If so, used another 
+        while (viewed_message[r])
+        {
+            r = rnd.Next(content_items.Count);
+        }
+
+        //increase our count, marked our message as viewed
+        total_viewed++;
+        viewed_message[r] = true;
+
+        //reset our viewed messages if the have all been viewed
+        if (total_viewed == content_items.Count)
+        {
+            viewed_message = new bool[content_items.Count];
+        }
+
+        //return the things.
         Debug.Log(content_items[r].content_prefab.ToString());
         return content_items[r];
     }
